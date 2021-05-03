e46cfa947c127cf965fdbed8b6dc75ab1d79b38e
risaksson <saygram@gmail.com>
2021-05-03 20:17:33 +0200

    Added repository

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "simple.h"
+
+int main() {
+    float vin[16], vout[16];
+    for (int i = 0; i < 16; ++i)
+        vin[i] = i;
+
+    ispc::simple(vin, vout, 16);
+
+    for (int i = 0; i < 16; ++i)
+        printf("%d: simple(%f) = %f\n", i, vin[i], vout[i]);
+}
